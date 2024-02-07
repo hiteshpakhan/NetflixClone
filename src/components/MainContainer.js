0e83a3c3668beb61c118454c46943663a48d7b7f
hiteshpakhan <hitesh5pakhan@gmail.com>
2024-02-07 13:44:53 +0530

    a

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { UseSelector, useSelector } from 'react-redux';
-
+import { useSelector } from 'react-redux';
+import VideoBackground from "./VideoBackground";
+import VideoTitle from "./VideoTital";
 
 const MainContainer = () => {
     const movies = useSelector(store => store.movies?.nowPlayingMovies)
+
+    if (!movies) return;  // most IMP this is called early return
+    //here if the movies is empty then it will return from here
+
+    const mainMovie = movies[0];  //here we will need first movie to show the on video on background
+    
+    const { original_title, overview, id } = mainMovie;
+    
   return (
-    <div>MainContainer</div>
+    <div>
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground movieId={id} />
+    </div>
   )
 }
 
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { API_OPTIONS } from '../utils/constants'
 
-const VideoBackground = () => {
+const VideoBackground = ({movieId}) => {
+
+  const getMovieVideo = async () => {
+    const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, API_OPTIONS);
+    const json = await data.json();
+    console.log("json data ==",json);
+
+    const filterData = json.results.filter((video)=> video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    console.log(trailer);
+  };
+
+  useEffect(() => {
+    getMovieVideo();
+  }, []);
+
   return (
     <div>VideoBackground</div>
   )
 }
 
-export default VideoBackground
+export default VideoBackground;
