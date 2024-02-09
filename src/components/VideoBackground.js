056bf6c3dccaf4b3e069bad1d335cdad30870f94
hiteshpakhan <hitesh5pakhan@gmail.com>
2024-02-09 15:02:55 +0530

    a

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,24 +1,21 @@
-import React, { useEffect } from 'react'
-import { API_OPTIONS } from '../utils/constants'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import useMovieTrailer from '../hooks/useMovieTrailer';
 
 const VideoBackground = ({movieId}) => {
+  const trailerVideo = useSelector(store => store.movies?.trailerVideo);
 
-  const getMovieVideo = async () => {
-    const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, API_OPTIONS);
-    const json = await data.json();
-    console.log("json data ==",json);
-
-    const filterData = json.results.filter((video)=> video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-  };
-
-  useEffect(() => {
-    getMovieVideo();
-  }, []);
+  useMovieTrailer(movieId);
 
   return (
-    <div>VideoBackground</div>
+    <div className='w-screen'>
+      <iframe className='w-screen aspect-video' 
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"}
+        title="YouTube video player" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        >
+      </iframe>
+    </div>
   )
 }
 
