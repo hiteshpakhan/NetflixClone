ae4a50755712b3abb9b1c073492d4a53a327ab49
hiteshpakhan <hitesh5pakhan@gmail.com>
2024-02-09 21:59:28 +0530

    a

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -2,10 +2,16 @@ import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowplayingMovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useTopRatedMovies from "../hooks/useTopRatedMovies";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
 
 const Browse = () => {
 
-  useNowPlayingMovies()
+  useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
+  useUpcomingMovies();
   
   return (
     <div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,5 @@ export const API_OPTIONS = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjU4ZWQwYjg4NTlmZTgyODYwMDgyMWQ0NTUxOTNmZiIsInN1YiI6IjY1YmUxYjg5YmE0ODAyMDE2MTY5NDYxZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QTmlGNkBtaBkTAHfIHbB8aBPMOzNuo9_JsGWR_lfLAA'
     }
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
